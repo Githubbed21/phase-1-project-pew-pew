7f3e0449ab6fe14204fa56dec85c935a572f5e70
Big Dog Dean <diazdean21@gmail.com>
2021-12-04 20:46:51 -0800

    changed the format of my HTML

---
+++ js/index.js
@@ -0,0 +1,21 @@
+//types of guns 
+
+//colt 1911 (45 ACP) https://images.gunsinternational.com/listings_sub/acc_87170/gi_101309216/Colt-1911-Lightweight-Commander-45-ACP-O4860XSE_101309216_87170_867951F04C28C232.jpg
+
+//glock 19 (9mm) https://th.bing.com/th/id/R.a8224ba6d0ddf5f5d5ff1a36801968f8?rik=P7k2wUaU9ajSpw&pid=ImgRaw&r=0
+
+// beretta m9 (9mm) https://www.filepicker.io/api/file/bcCzaU04R9GQlU77WcfG
+
+// colt python (357 mag) https://th.bing.com/th/id/R.c1b0ad407f38e003609e427300a00ec4?rik=3hgHuQFvPugAwg&pid=ImgRaw&r=0
+
+// sig saure (9mm) https://th.bing.com/th/id/OIP.b_m5Nt-bZ7o4ndTpOOUSbgHaFz?pid=ImgDet&rs=1
+
+// AR-15 (5.56) https://th.bing.com/th/id/R.fdcae43942a20b81cae6d4409d4ccabd?rik=yJ1dLUVEGRfkdQ&riu=http%3a%2f%2f2ht1mik98ka4dogie28vqc4y.wpengine.netdna-cdn.com%2fwp-content%2fuploads%2f2017%2f02%2fDD5V1_01d.jpg&ehk=N6C0MjXL2g9F4iVPyDeVGDampjrT%2fP8tmZ5eXJgre4o%3d&risl=&pid=ImgRaw&r=0
+
+// AK-47 (7.62) https://fh-sites.imgix.net/sites/1663/2018/08/29161340/shutterstock_102492452_1.jpg?auto=compress%2Cformat&w=700&h=700&fit=max
+
+//scar 17 (.308) https://fnamerica.com/wp-content/uploads/2012/09/SCAR-17S-FDE_Rotators_3-1800x897.png
+
+// 30 ot 6 https://th.bing.com/th/id/R.63e2338ef635d092e19d5363263480b2?rik=PWOj9zKfo7hkmQ&riu=http%3a%2f%2fmedia.liveauctiongroup.net%2fi%2f20859%2f19846431_2.jpg%3fv%3d8D14AB0D92EC0D0&ehk=BLyZZLSfp5fb24jsgGP%2bq%2bZJkpHup2qPoNRqZJFmtk4%3d&risl=&pid=ImgRaw&r=0
+
+// barrett (50 BMG) https://th.bing.com/th/id/R.cae26a3300c4b1a248aa6dfe88467239?rik=wNtusZ%2b599DpTw&pid=ImgRaw&r=0
